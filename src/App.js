568552d682ae61c871672a3d9521400423b797be
thaian161 <thaian161@yahoo.com>
2022-07-12 14:33:09 -0500

    completed all fancy buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import AngryButton from './components/AngryButton';
 import LightSwitchButton from './components/LightSwitchButton';
 import TextRepeaterButton from './components/TextRepeaterButton';
 import CounterButton from './components/CounterButton';
 
+
 function App() {
+  const [light, setLight] = useState("off");
+  const switchLight = () => setLight(light === "on" ? "off" : "on");
+
+  const dark = (light === 'off') ? 'dark' : '';
+
   return (
-    <div className="App">
+    <div className={`App ${dark}`}>
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton />
+        <LightSwitchButton light={light} switchLight={switchLight}/>
         <TextRepeaterButton />
       </section>
     </div>
--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,24 +1,16 @@
-import { useState } from 'react';
+import React from 'react';
+//import { useState } from "react";
 
-function LightSwitchButton() {
-  const [light, setLight] = useState('off');
-
-  const handleClick = () => setLight(light === 'on' ? 'off' : 'on');
+function AngryButton(props) {
+  const {light, switchLight} = props
+  const handleClick = () => switchLight();
 
   return (
     <button onClick={handleClick} className="LightSwitchButton">
-      {light === 'on' && (
-        <span>
-          <i>💡</i> I'm on!
-        </span>
-      )}
-
-      {light === 'off' && (
-        <span className="off">
-          <i>💡</i> I'm off!
-        </span>
-      )}
+    {light === "on" && <span><i>💡</i> I'm on!</span>}
+    {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
-  );
+  )
 }
-export default LightSwitchButton;
+
+export default AngryButton;
